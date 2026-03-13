d3ab78e7cb6b6444480955c0e3c1d41cafce67ec
Pushkar Mondal <nishitm060@gmail.com>
2026-03-13 19:43:40 +0530

    Post request of creating jobs

---
+++ src/api/server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { v4 as uuid } from "uuid";
+import { redis } from "../redis/redisConnection";
+
+const app = express();
+const PORT = 3004;
+const STREAM_KEY = "job";
+
+app.use(express.json());  
+
+app.post("/jobs", async (req, res) => {
+  const job = {
+    id: uuid(),
+    type: req.body.type,
+    payload: req.body.payload,
+    attempts: 0,
+    maxAttempts: 3,
+    createdAt: Date.now(),
+  };
+  await redis.xadd(
+    STREAM_KEY,
+    "*",
+    "data",
+    JSON.stringify(job)
+  )
+  res.json({jobId: job.id})
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
