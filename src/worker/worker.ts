1e0584df6e510247a13278c3533131257008146d
Pushkar Mondal <nishitm060@gmail.com>
2026-03-13 20:16:36 +0530

    DLQ implemented

--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -1,14 +1,46 @@
 import { redis } from "../redis/redisConnection";
 
 const STREAM = "jobs";
+const DLQ_STREAM = "jobs.dlq"
+
 const GROUP = "workers";
 const CONSUMER = "worker-1";
 const BLOCK = "BLOCK" as const;
 
 async function processJob(job: any) {
   console.log("Processing job: ", job.id);
+  
+  if(Math.random() < 0.3) {
+    throw new Error("Random failure");
+  }
+  
   await new Promise((r) => setTimeout(r, 2000));
   console.log("Finished processing job: ", job.id);
+}
+
+async function sendToDLQ(job: any, error: any) {
+  const failedJob = {
+    ...job,
+    failedAt: Date.now(),
+    error: error.message
+  }
+  await redis.xadd(
+    DLQ_STREAM,
+    "*",
+    "data",
+    JSON.stringify(failedJob)
+  )
+}
+
+async function retryJob(job: any) {
+  job.attempts += 1;
+  await redis.xadd(
+    STREAM,
+    "*",
+    "data",
+    JSON.stringify(job)
+  )
+  console.log("Retry scheduled:", job.id)
 }
 
 async function main() {
@@ -34,8 +66,17 @@ async function main() {
       try {
         await processJob(job);
         await redis.xack(STREAM, GROUP, id);
+        console.log("Job processed successfully: ", job.id);
       } catch (error) {
         console.error("Error processing job: ", error);
+        
+        await redis.xack(STREAM, GROUP, id)
+        
+        if(job.attempts >= job.maxAttempts) {
+          await sendToDLQ(job, error);
+        } else {
+          await retryJob(job);
+        }
       }
     }
   }
